c230b6c22f9b09819452b37d026d0a14c4063458
ryanpavlik24 <ryan.pavlik@tamu.edu>
2023-11-15 18:33:42 -0600

    Template

---
+++ template.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+<head>
+  <link rel="stylesheet" href="template.css">
+</head>
+
+<ul>
+  <li><a href="dummy1.php">Dummy 1</a></li>
+  <li><a href="dummy2.php">Dummy 2</a></li>
+  <li><a href="dummy3.php">Dummy 3</a></li>
+  <li style="float:right"><a href="login.php">Logout</a></li>
+</ul>
+
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<form action="database.php" method="post">
+First Name: <input type="text" name="fNameInput"><br>
+Middle Initial: <input type="text" name="mNameInput"><br>
+Last Name: <input type="text" name="lNameInput"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
